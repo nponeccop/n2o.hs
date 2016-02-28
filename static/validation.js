6a0b3d9e49af0aab4e2ecc688e4376f20ad706ab
Andre <persiantiger@yandex.ru>
2016-02-28 03:01:23 +0100

    Update validation.js

--- static/validation.js
+++ static/validation.js
@@ -1,4 +1,6 @@
 
+// N2O Validation
+
 function validateSources(list) {
     return list.reduce(function(acc,x) {
         var event = new CustomEvent('validation');
@@ -9,18 +11,6 @@ function validateSources(list) {
         console.log(res);
         if (el) el.style.background = res ? '' : 'pink';
         return res && acc; },true); }
-
-function validateLength(e, minlength, maxlength) {
-    var field = e.detail;
-    return field.length >= minlength && field.length <= maxlength; }
-
-function validateMin(e, min) {
-    var field = e.detail;
-    var re = /^\d{1,}$/;
-    console.log(re.test(field));
-    return (!re.test(field)) ? false : parseFloat(field) > min; }
-
-// IE polyfill
 
 (function () {
    function CustomEvent ( event, params ) {
