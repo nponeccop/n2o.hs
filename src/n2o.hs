4a1983cc3f80f48cd051314d6f8bdafc6e48b42f
Andre <persiantiger@yandex.ru>
2016-02-28 11:00:18 +0100

    closes #16

--- src/n2o.hs
+++ src/n2o.hs
@@ -33,9 +33,9 @@ invalidName name = T.null name || T.any isPunctuation name || T.any isSpace name
 clientExists state name = not . I.null . getEQ (Just name) . coSet <$> readMVar state
 
 handle state entry [AtomTerm "LOGON", BinaryTerm name]
-    | invalidName $ lazyDecodeUtf8 name = alert entry "Name cannot contain punctuation or whitespace, and cannot be empty"
+  | invalidName $ T.pack $ lazyDecodeUtf8 name = alert entry "Name cannot contain punctuation or whitespace, and cannot be empty"
     | otherwise = do
-        let dname = T.unpack $ lazyDecodeUtf8 name
+        let dname = lazyDecodeUtf8 name
         print $ "login: " <> dname
         ce <- clientExists state dname
         if ce 
@@ -44,21 +44,25 @@ handle state entry [AtomTerm "LOGON", BinaryTerm name]
                 send entry $ call "joinSession" ""
                 setState state (eSocketId entry) $ Just dname
                 clients <- loggedOn state
-                let foo = lazyEncodeUtf8 . T.pack $ concatMap ((\x -> "<li>" <> x <> "</li>") . fromJust . eUser) clients
+                let foo = lazyEncodeUtf8 $ concatMap ((\x -> "<li>" <> x <> "</li>") . fromJust . eUser) clients
                 broadcast (call "$('#users').html" foo <> call "log" (name <> " joined")) clients
         
-handle state _entry [AtomTerm "MSG", BinaryTerm text]
+handle state entry [AtomTerm "MSG", BinaryTerm text]
     = do
         clients <- loggedOn state
-        sendMessage text clients
+        let ctext = getEncodedUser entry <> ": " <> text
+        sendMessage ctext clients
 
 handle state entry [AtomTerm "N2O_DISCONNECT"]
     = do
         clients <- loggedOn state
         let foo = concatMap ((\x -> "<li>" <> fromString x <> "</li>") . fromJust . eUser) clients
-        broadcast (call "$('#users').html" (fromString foo) <> call "log" (fromMaybe "Someone" (fromString <$> eUser entry) <> " disconnected")) clients
+        broadcast (call "$('#users').html" (fromString foo) <> call "log" (getUser entry <> " disconnected")) clients
 
 handle _state _entry _ = putStrLn "Protocol violation"
 
-lazyDecodeUtf8 = decodeUtf8 . toStrict
-lazyEncodeUtf8 = fromStrict . encodeUtf8
+getUser entry = fromMaybe "Someone" $ fromString <$> eUser entry
+getEncodedUser = lazyEncodeUtf8 . getUser
+
+lazyDecodeUtf8 = T.unpack . decodeUtf8 . toStrict
+lazyEncodeUtf8 = fromStrict . encodeUtf8 . T.pack
