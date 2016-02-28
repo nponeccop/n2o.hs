1edb74f78c18d45a6098ce60d83e46de813279de
Andre <persiantiger@yandex.ru>
2016-02-28 02:59:37 +0100

    Update bullet.js

--- static/bullet.js
+++ static/bullet.js
@@ -1,166 +1,33 @@
 
-// N2O Bullet
+// WebSocket Transport
 
-function bullet(url) {
+$ws = { heart: true, interval: 4000,
+        creator: function(url) { return window.WebSocket ? new window.WebSocket(url) : false; },
+        onheartbeat: function() { this.channel.send('PING'); } };
 
-    var CONNECTING = 0;
-    var OPEN = 1;
-    var CLOSING = 2;
-    var CLOSED = 3;
+// N2O Reliable Connection
 
-    var transports = {
+$conn = { onopen: nop, onmessage: nop, onclose: nop,
+          send:  function(data)   { if (this.port.channel) this.port.channel.send(data); },
+          close: function()       { if (this.port.channel) this.port.channel.close(); } };
 
-        websocket: function() {
-            var transport = null;
-            if (window.WebSocket) { transport = window.WebSocket; }
-            if (transport) { return {'heart': true, 'transport': transport}; }
-            return null;
-        },
+ct = 0;
+transports = [ $ws ];
+heartbeat = null;
+reconnectDelay = 1000;
+maxReconnects = 100;
 
-        xhrPolling: function() {
-            var timeout;
-            var xhr;
-
-            nextPoll();
-
-            var fake = {
-                readyState: CONNECTING,
-
-                receive: function(data) {
-                    if (fake.readyState == CONNECTING) { fake.readyState = OPEN; fake.onopen(fake); }
-                    if (data.length != 0) { fake.onmessage({'data': data }); }
-                    if (fake.readyState == OPEN) { nextPoll(); }
-                },
-
-                send: function(data) {
-                    if (this.readyState != CONNECTING && this.readyState != OPEN) return false;
-                    var fakeurl = url.replace('ws:', 'http:').replace('wss:', 'https:');
-                    var request = new XMLHttpRequest();
-                    request.open('POST',fakeurl,true);
-                    request.setRequestHeader('Content-Type',
-                        'application/x-www-form-urlencoded; charset=utf-8');
-                    request.setRequestHeader('X-Socket-Transport','xhrPolling');
-                    request.onload = function() { fake.receive(request.response); };
-                    request.send(data);
-                    return true;
-                },
-                close: function(){
-                    this.readyState = CLOSED;
-                    xhr.abort();
-                    clearTimeout(timeout);
-                    fake.onclose();
-                },
-                onopen: function(){},
-                onmessage: function(){},
-                onerror: function(){},
-                onclose: function(){}
-            };
-
-            function poll(pooling){
-                var fakeurl = url.replace('ws:', 'http:').replace('wss:', 'https:');
-                var request = new XMLHttpRequest();
-                request.open('GET',fakeurl,true);
-                request.setRequestHeader('Content-Type',
-                    'application/x-www-form-urlencoded; charset=utf-8');
-                request.setRequestHeader('X-Socket-Transport','xhrPolling');
-                request.onload = function() { fake.receive(request.response); };
-                request.onerror = function() { fake.onerror(); };
-                request.send({});
-            }
-
-            function nextPoll() { timeout = setTimeout(function(){poll();}, 1000); }
-
-
-            return {'heart': false, 'transport': function(){ return fake; fake.nextPoll(); }};
-        }
-    };
-
-    var tn = 0;
-    function next() {
-        var c = 0;
-
-        for (var f in transports) {
-            if (tn == c) {
-                var t = transports[f]();
-                if (t) { var ret = new t.transport(url); ret.heart = t.heart; return ret; }
-                tn++;
-            }
-            c++;
-        }
-        return false;
-    }
-
-    var stream = new function() {
-        var isClosed = true;
-        var readyState = CLOSED;
-        var heartbeat;
-        var delay = 80;
-        var delayDefault = 80;
-        var delayMax = 10000;
-
-        var transport;
-        function init() {
-
-            isClosed = false;
-            readyState = CONNECTING;
-            transport = next();
-
-            if (!transport) {
-                delay = delayDefault;
-                tn = 0;
-                stream.ondisconnect();
-                setTimeout(function(){init();}, delayMax);
-                return false;
-            }
-
-            transport.onopen = function() {
-                delay = delayDefault;
-
-                if (transport.heart) { 
-                    heartbeat = setInterval(function(){stream.onheartbeat();}, 4000);
-                }
-
-                if (readyState != OPEN) { readyState = OPEN; stream.onopen(); }
-            };
-
-            transport.onclose = function() {
-                if (isClosed) { return; }
-
-                transport = null;
-                clearInterval(heartbeat);
-
-                if (readyState == CLOSING){
-                    readyState = CLOSED;
-                    stream.onclose();
-                } else {
-                    if (readyState == CONNECTING) tn++;
-                    delay *= 2;
-                    if (delay > delayMax) { delay = delayMax; }
-                    isClosed = true;
-                    setTimeout(function() { init(); }, delay);
-                }
-            };
-            transport.onerror = transport.onclose;
-            transport.onmessage = function(e){
-            stream.onmessage(e);
-            };
-        }
-
-        init();
-
-        this.onopen = function(){};     this.oninit = function(){};
-        this.onmessage = function(){};  this.ondisconnect = function(){ initialized = false; };
-        this.onclose = function(){};    this.onheartbeat = function(){ return this.send('PING'); };
-
-        this.setURL = function(newURL) { url = newURL; };
-        this.send = function(data){
-            if (transport) return transport.send(data); else return false;
-        };
-        this.close = function(){
-            readyState = CLOSING;
-            if (transport) transport.close();
-        };
-    };
-
-    return stream;
-}
+function nop() { }
+function bullet(url) { $conn.url = url; return $conn; }
+function xport() { return maxReconnects <= ct ? false : transports[ct++ % transports.length]; }
+function reconnect() { setTimeout(function() { connect(); }, reconnectDelay); }
+function next() { $conn.port = xport(); return $conn.port ? connect() : false; }
+function connect() {
+    $conn.port.channel = $conn.port.creator($conn.url);
+    if (!$conn.port.channel) return next();
+    $conn.port.channel.onmessage = function(e) { $conn.onmessage(e); };
+    $conn.port.channel.onopen = function() {
+        if ($conn.port.heart) heartbeat = setInterval(function(){$conn.port.onheartbeat();}, $conn.port.interval);
+        $conn.onopen(); };
+    $conn.port.channel.onclose = function() { $conn.onclose(); clearInterval(heartbeat); reconnect(); };
+    return $conn; }
