809212dd02e1e6f7c5d29257460e08e5cb991f22
Andrii Melnykov <andy.melnikov@gmail.com>
2016-03-02 00:44:47 +0200

    Fix: static-tiny crash
    Connect Middleware API was implemented incorrectly, so
    finalHandler didn't have arguments to action upon

--- contrib/static-tiny/index.js
+++ contrib/static-tiny/index.js
@@ -1,16 +1,15 @@
 var s = require('./static')
 
-var http = require('http');
-
 function finalHandler(req, res) {
+	console.log({arg : arguments})
     res.setHeader('Content-Type', 'text/plain');
     res.end("Couldn't handle the request")
 }
 
-http
-	.createServer(require('./static')(process.cwd(), { maxAge: 0 }, finalHandler))
+var h =	require('./static')(process.cwd(), { maxAge: 0 }) 
+require('http')
+	.createServer(function (req, res) { h(req, res, finalHandler.bind(null, req, res)) })
 	.listen(8000)
 
 console.log("Static files are served at http://localhost:8000")
-
 
