f85fed7b4ad16ab97e3ca0030e1a6e8ca18ec679
Andrii Melnykov <andy.melnikov@gmail.com>
2016-04-10 07:44:33 +0300

    Internal: refactoring of StaticWebServer

--- contrib/static-web-server/StaticWebServer.hs
+++ contrib/static-web-server/StaticWebServer.hs
@@ -3,40 +3,32 @@
 import Network.HTTP.Server
 import Network.HTTP.Server.Logger
 import Network.URL as URL
-import Codec.Binary.UTF8.String
 import Control.Exception(catch,IOException)
 import System.FilePath
 import Data.Map.Strict
 import qualified Data.ByteString.Char8 as BS
 
-main :: IO ()
-main = serverWith defaultConfig { srvHost = "0.0.0.0", srvLog = stdLogger, srvPort = 8000 }
-     $ \_ url request ->
+main = serverWith defaultConfig { srvHost = "0.0.0.0" } $ \_ url request -> serve (rqMethod request) (url_path url)
 
-  case rqMethod request of
+serve GET path = returnFileContent (BS.pack $ takeExtension path) path `catch` (\(_ :: IOException) -> return $ err_response NotFound)
 
-    GET -> do
-      let ext = takeExtension (url_path url)
-      putStrLn $ url_path url
-      returnFileContent (BS.pack ext) url `catch` (\(_ :: IOException) -> return $ err_response NotFound)
+serve _ _ = return $ err_response MethodNotAllowed
 
-    _ -> return $ err_response MethodNotAllowed
-
-sendText :: BS.ByteString -> StatusCode -> BS.ByteString -> Response BS.ByteString
-sendText e s v    = insertHeader HdrContentType (chooseCType e)
-                $ insertHeader HdrContentLength (show (BS.length v))
-                $ insertHeader HdrContentEncoding "UTF-8"
-                $ insertHeader HdrContentEncoding "text/plain"
-                $ (respond s :: Response BS.ByteString) { rspBody = v }
-
-chooseCType ext = findWithDefault "application/octet-stream" ext ctm
+sendText e v
+    = Response
+    { rspCode = (2,0,0)
+    , rspReason = "OK"
+    , rspHeaders =
+      [ Header HdrContentType $ findWithDefault "application/octet-stream" e ctm
+      , Header HdrContentLength $ show $ BS.length v
+      , Header HdrContentEncoding "UTF-8"
+      ]
+    , rspBody = v
+    }
 
 ctm = fromList [ (".js","application/x-javascript")
                , (".css", "text/css")
                , (".html", "text/html")
                ]
 
-returnFileContent :: BS.ByteString -> URL -> IO (Response BS.ByteString)
-returnFileContent e a = do
-  b <- BS.readFile $ url_path a
-  return $ sendText e OK b
+returnFileContent e a = sendText e <$> BS.readFile a
