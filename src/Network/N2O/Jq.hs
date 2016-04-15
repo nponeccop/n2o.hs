b98d7a922bf80eb654e0d5323918c8e7e7224b9c
Andrii Melnykov <andy.melnikov@gmail.com>
2016-04-15 05:17:17 +0300

    New: session cookie using Jq

--- src/Network/N2O/Jq.hs
+++ src/Network/N2O/Jq.hs
@@ -4,8 +4,13 @@ module Network.N2O.Jq where
 import Language.HJavaScript.Syntax
 
 data El = El deriving (Show)
+data Doc = Doc deriving (Show)
 
 instance IsClass El
+instance IsClass Doc
+
+doc :: Exp Doc
+doc = JConst "document"
 
 qi :: String -> Exp El
 qi s = JCall (JConst "qi") (JString s)
@@ -15,3 +20,4 @@ jqSetProp obj prop val = ExpStmt $ JAssign (JDerefVar obj prop) val
 jqHide id = jqSetProp (qi id) "style.display" $ JString "none"
 jqShow id = jqSetProp (qi id) "style.display" $ JString "block"
 
+setSid sid = jqSetProp doc "cookie" . JString $ "n2o_sid=" ++ sid
